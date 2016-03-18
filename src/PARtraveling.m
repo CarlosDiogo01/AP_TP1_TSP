25bd7e6d36ca956d9e9fadce33ee8fc5f96798ad
Bruno Barbosa <brunobarbosa.caos@gmail.com>
2016-03-18 15:36:02 +0000

    added new version of PARtravelling file
    -creates a zero cost route
    -measures the time spent on each situation

--- src/PARtraveling.m
+++ src/PARtraveling.m
@@ -1,8 +1,8 @@
 function PARtraveling(n,procs)
 
-% This is a simulation of a parallel execution on p processors of the code
+% This is a simulation of a parallel execution on P processors of the code
 % traveling which uses simulated annealing to find the shortest route to go
-% through each one of n towns
+% through each one of N towns
 % Each process executes the same code with different random
 % numbers and, in general, will find different answers with different
 % costs. The parallel solution is thus the answer with smaller cost.
@@ -17,23 +17,43 @@ for i=1:n
         D(i,j)=sqrt((x(i)-x(j))^2+(y(i)-y(j))^2);
     end
 end
+% set up the zero cost route
+for i=1:n
+    j = i+1;
+    if(j == n+1)
+        j=1;
+    end
+    D(i,j) = 0;
+    D(j,i) = 0;
+end
+D
 clf
 % with simulated annealing
+% measure time
+sai = tic;
 for p=1:procs
     [Tdist(p),route]=traveling(x,y,D); 
     subplot(2,procs,p);
     plot([x(route),x(route(1))],[y(route),y(route(1))],'r',x(route),y(route),'o')
     legend(num2str(Tdist(p)))
 end
+saf = toc(sai);
 
  
-% without simulated annealing  
+% without simulated annealing
+wsai = tic;
 for p=1:procs
     [Tdist2(p),route2]=traveling2(x,y,D);
     subplot(2,procs,procs+p);
     plot([x(route2),x(route2(1))],[y(route2),y(route2(1))],'k',x(route2),y(route2),'o')
     legend(num2str(Tdist2(p)))
 end
+wsaf = toc(wsai);
+
+fprintf('Time spent with SA %f seconds\n',saf);
+fprintf('Time spent without SA %f seconds\n',wsaf);
 
 [Tdist' Tdist2']
+
+
 
